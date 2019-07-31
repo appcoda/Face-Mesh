522ce58d995b7c7c2ea4a504daafe74991693270
Simon Ng <simonng@appcoda.com>
2019-07-31 10:51:20 +0800

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Face Mesh
- Use ARKit to detect and track the user's face
+# Face Mesh with ARKit
+ Learn how to use ARKit to detect and track the user's face
+
+For the full tutorial, please refer to this link:
+
+https://www.appcoda.com/arkit-face-tracking
---
+++ True Depth/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  True Depth
+//
+//  Created by Sai Kambampati on 2/23/19.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var faceLabel: UILabel!
+    @IBOutlet weak var labelView: UIView!
+    var analysis = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        labelView.layer.cornerRadius = 10
+        
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        guard ARFaceTrackingConfiguration.isSupported else {
+            fatalError("Face tracking is not supported on this device")
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARFaceTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let faceMesh = ARSCNFaceGeometry(device: sceneView.device!)
+        let node = SCNNode(geometry: faceMesh)
+        node.geometry?.firstMaterial?.fillMode = .lines
+        return node
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        if let faceAnchor = anchor as? ARFaceAnchor, let faceGeometry = node.geometry as? ARSCNFaceGeometry {
+            faceGeometry.update(from: faceAnchor.geometry)
+            expression(anchor: faceAnchor)
+            
+            DispatchQueue.main.async {
+                self.faceLabel.text = self.analysis
+            }
+            
+        }
+    }
+    
+    func expression(anchor: ARFaceAnchor) {
+        let smileLeft = anchor.blendShapes[.mouthSmileLeft]
+        let smileRight = anchor.blendShapes[.mouthSmileRight]
+        let cheekPuff = anchor.blendShapes[.cheekPuff]
+        let tongue = anchor.blendShapes[.tongueOut]
+        self.analysis = ""
+        
+        if ((smileLeft?.decimalValue ?? 0.0) + (smileRight?.decimalValue ?? 0.0)) > 0.9 {
+            self.analysis += "You are smiling. "
+        }
+        
+        if cheekPuff?.decimalValue ?? 0.0 > 0.1 {
+            self.analysis += "Your cheeks are puffed. "
+        }
+        
+        if tongue?.decimalValue ?? 0.0 > 0.1 {
+            self.analysis += "Don't stick your tongue out! "
+        }
+    }
+}
